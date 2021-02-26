b9f51224d1b377506433170c56f860f25794948e
vigneshvicky25 <vickychandran1992@gmail.com>
2021-02-26 12:42:11 +0530

    Pushing code

---
+++ src/org/test/DayTwo.java
@@ -0,0 +1,36 @@
+package org.test;
+
+public class DayTwo {
+	
+public static void main(String[] args) {
+		
+//		String s[] = new String[6];
+//		s[0]="Vicky";
+//		s[1]="Sam";
+//		s[4]="Ban";
+//		s[5]="Dan";
+//		
+//		System.out.println(s[3]);
+//		System.out.println(s.length);
+//		
+//		
+//		for (String l : s) {
+//			System.out.println(l);
+//			
+//		}
+		
+		int a[][]= new int[2][2];
+		 a [0][1]=99;
+		 a[1][1]=77;
+		 
+		 
+		 for (int i = 0; i < a.length; i++) {
+			 
+			 for (int j = 0; j < a.length; j++) {
+				 System.out.println(a[i][j]);
+				
+			}
+			
+		}
+
+}}
